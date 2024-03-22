98f70ff810231a673d212de7abc8904a707e50f6
ermek <sagynbekoff@icloud.com>
2024-03-22 14:37:45 -0500

    msg

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -2,7 +2,7 @@ describe("CSSLocators", () => {
     it("csslocators", () => {
         cy.visit("https://google.com")
         cy.get("#APjFqb").type("Cars{enter}")
-    // cy.get("div[class='FPdoLc lJ9FBc']").click()
+        // cy.get("div[class='FPdoLc lJ9FBc']").click()
         cy.get("#result-stats").contains("About")
 
     })
