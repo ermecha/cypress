a5e250f7bda93d4effa1fca6934ce6633e8bd60f
ermek <sagynbekoff@icloud.com>
2024-03-23 12:46:30 -0500

    m

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -8,16 +8,26 @@ describe("Assetions demo", () => {
         // cy.url().should("include", "orangehrmlive.com")
         // .should("include", "orangehrmlive.com")
         // .should("contain", "web/index.php")
-         cy.url().should("include", "orangehrmlive.com")
-             .and("include", "orangehrmlive.com")
-             .and("contain", "web/index.php")
-         cy.title().should("contain", "Orange")
-         cy.get("img[alt='company-branding']").should("be.visible").and("exist")
-         cy.get("a").should("have.length","5")
-         cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type("Admin")
-         cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").should("have.value","Admin")
-         cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type("admin123{enter}")
-         
-         
+        cy.url().should("include", "orangehrmlive.com")
+            .and("include", "orangehrmlive.com")
+            .and("contain", "web/index.php")
+        cy.title().should("contain", "Orange")
+        cy.get("img[alt='company-branding']").should("be.visible").and("exist")
+        cy.get("a").should("have.length", "5")
+        cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type("Admin")
+        cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").should("have.value", "Admin")
+        cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type("admin123{enter}")
+    })
+    it("Expect", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type("Admin")
+        cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type("admin123{enter}")
+        let expName = "Bob Tester";
+        cy.get(".oxd-userdropdown-name").then( (x)=>{
+              let actualName=x.text()
+             expect(actualName).to.equal(expName)
+         })
+      
+
     })
 })
