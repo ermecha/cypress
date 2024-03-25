8e601a89ab78e67f55f5bc612179c1c484e922d0
ermek <sagynbekoff@icloud.com>
2024-03-24 20:19:39 -0500

    m

--- cypress/e2e/Tabs.cy.js
+++ cypress/e2e/Tabs.cy.js
@@ -3,6 +3,11 @@
 describe("Handle Tab",()=>{
     it("Tabs",()=>{
       cy.visit("https://practice.cydeo.com/windows")
-      cy.get("a[href='/windows/new']").click()
+      cy.get("a[href='/windows/new']").invoke("removeAttr","target").click()
+      cy.wait(5000)
+      cy.go("back")
+    })
+    it.only("href",()=>{
+        cy.visit("https://practice.cydeo.com/windows/new")
     })
 })
